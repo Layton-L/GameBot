64f92e36cf6debdbb44253f82f055a8a1a1c6611
Aleksey Vorobyov <67465529+Layton-L@users.noreply.github.com>
2022-07-06 20:45:15 +0300

    Update table.sql

--- resources/sqls/table.sql
+++ resources/sqls/table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    telegram_id INTEGER NOT NULL,
-    balance INTEGER DEFAULT 1000,
+    id           INTEGER PRIMARY KEY,
+    telegram_id  INTEGER NOT     NULL,
+    balance      INTEGER DEFAULT 1000,
     bank_balance INTEGER DEFAULT 100,
-    level INTEGER DEFAULT 1,
-    exp INTEGER DEFAULT 0,
-    time INTEGER NOT NULL,
-    bonus_time INTEGER NOT NULL
+    level        INTEGER DEFAULT 1,
+    exp          INTEGER DEFAULT 0,
+    time         INTEGER NOT     NULL,
+    bonus_time   INTEGER NOT     NULL
 )
