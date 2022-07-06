e7ab9e63b7120b46a1dae7658157dcf72ebbfaab
Aleksey Vorobyov <67465529+Layton-L@users.noreply.github.com>
2022-07-06 22:44:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+from os import path
+
+
 TELEGRAM_TOKEN = ""
 ADMINS_IDS = []
 
-RESOURCES_FOLDER = "resources\\"
+RESOURCES_FOLDER = path.dirname(__file__) + "\\resources\\"
 SQLS_FOLDER = RESOURCES_FOLDER + "sqls\\"
 DATABASE_NAME = RESOURCES_FOLDER + "bot.db"
